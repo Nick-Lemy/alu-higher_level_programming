9414340b195b2676728b3f1c974ecf4af1e908ce
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-27 13:37:03 +0200

    Task 2 attempt 7

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 import sys
-print("{}".format("".join(chr(i) for i in range(ord('a'), ord('z')+1))), end="")
+#print("{}".format("".join(chr(i) for i in range(ord('a'), ord('z')+1))), end="")
+
+for i in range(ord('a'), ord('z')+1):
+    print("{}".format("".join(chr(i))), end="")
