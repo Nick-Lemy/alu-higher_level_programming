a1d27e5919530fed473e219e50425ff76b831559
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-27 18:28:44 +0200

    Task 6 attempt 4

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        if int("{}{}".format(i,j)) < 89:
-            print("{}{}".format(i,j), end=", ")
+        if int("{}{}".format(i, j)) < 89:
+            print("{}{}".format(i, j), end=", ")
         else:
-            print("{}{}".format(i,j), end="\n")
+            print("{}{}".format(i, j), end="\n")
