d9d7545e445e06077e972bbc432db62840eaa73e
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-27 15:56:56 +0200

    Task 3 attempt 1

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for i in range(ord('a'), ord('z')+1):
+    if i != ord('q') and i != ord('e'):
+        print("{}".format("".join(chr(i))), end="")
+    else:
+        print("", end="")
