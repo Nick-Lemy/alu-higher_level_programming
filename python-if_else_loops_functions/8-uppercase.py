b2fc85bc9849ba3010dd5c3f6ba29b492eb3a25f
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-27 19:04:46 +0200

    Task 9 attempt 1

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for char in s:
+        if 'a' <= char <= 'z':
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+        else:
+            uppercase_char = char
+        print(uppercase_char, end="")
+    print()
