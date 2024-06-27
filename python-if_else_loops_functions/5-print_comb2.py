6798dd5cb69cd6a7847066f7e751dfa93054cdbf
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-27 16:31:24 +0200

    Task 5 attempt 2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 10:
-        print(f"0{i}", end=", ")
+        print("0{}".format(i), end=", ")
     else:
-        print(f"{i}", end=", ")
+        print("{}".format(i), end=", ")
 
