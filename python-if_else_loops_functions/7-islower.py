a36f2f6b89726155868e455429b873466771caee
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-27 18:49:05 +0200

    Task 8 attempt 4

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 
 def islower(c):
-    return len(c) == 1 and ord('a') <= ord(c) <= ord('z')
-    islower = __import__('7-islower').islower
-
-    print("a is {}".format("lower" if islower("a") else "upper"))
-    print("H is {}".format("lower" if islower("H") else "upper"))
-    print("A is {}".format("lower" if islower("A") else "upper"))
-    print("3 is {}".format("lower" if islower("3") else "upper"))
-    print("g is {}".format("lower" if islower("g") else "upper"))
+    return ord('a') <= ord(c) <= ord('z')
