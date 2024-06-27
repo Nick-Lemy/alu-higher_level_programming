352b3ab1c404c72ed899609a655e6192f22b18cc
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-27 16:39:35 +0200

    Task 5 attempt 7

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
+    if i == 99:
+        print("{}".format(i), end="")
+    elif i < 10:
         print("0{}".format(i), end=", ")
     else:
-        print("{}".format(i), end=", " if i < 99 else pass)
+        print("{}".format(i), end=", ")
