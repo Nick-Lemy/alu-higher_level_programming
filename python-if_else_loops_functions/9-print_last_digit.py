0b2f1594787254e49f3d5c732e3d198f80bf18e7
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-27 19:42:09 +0200

    task 10 attempt 7

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    # Get the absolute value of the number to handle negative numbers
-    last_digit = abs(number) % 10
-    print(last_digit)
-    return last_digit 
+    return print(abs(number) % 10, end='')
