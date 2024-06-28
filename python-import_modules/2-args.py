92b5e787ddfe63b5759c24442593907f47bacf60
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-28 13:18:10 +0200

    Task 2 Attempt 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import  sys
 if __name__ == "__main__":
-   # print("{} arguments".format(len(sys.argv) - 1))
     if len(sys.argv) - 1 == 1:
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
