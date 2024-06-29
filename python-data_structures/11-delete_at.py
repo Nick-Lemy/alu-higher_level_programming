5c7665c0593af58068a55652b6e5fe45f513caab
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-29 17:32:48 +0200

    Task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,11 +3,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
     else:
-        result =[]
-        for i in my_list:
-            if i != my_list[idx]:
-                result.append(i)
-            else:
-                pass
-        my_list = result
+        del my_list[idx]
         return my_list
