3a1af9aa1222d038885269b6b8689eeb0e25b9df
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-29 14:32:07 +0200

    Task 5

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new = ''
+    for i in my_string:
+        if i == 'c' or i == '':
+            i = ''
+            new += i
+        else:
+            new += i
+    return new
