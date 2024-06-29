0a14d6759a7b7d7dbef0c6e9eaec5fce7b3c5a25
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-29 16:44:08 +0200

    Task 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if len(mylist) > 0:
+    if len(my_list) > 0:
         result = []
-        for i in new_list:
+        for i in result:
             if i % 2 == 0:
                 result.append(i)
             else:
