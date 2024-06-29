f063ac6d43061bf565bc124460114a822f9330a7
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-29 16:42:23 +0200

    Task 10

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if mylist > 0:
+        result = []
+        for i in new_list:
+            if i % 2 == 0:
+                result.append(i)
+            else:
+                pass
+        return result
+    else:
+        return my_list
