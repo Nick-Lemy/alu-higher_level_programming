161665dc4e2c5b8950fdd818104bc5b40e611060
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-29 13:38:53 +0200

    Task 3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[1, 2, 3, 4, 5, 6, 7, 8, 9]):
+def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     for i in my_list:
-        print("{:2d}".format(i))
+        print("{:d}".format(i))
