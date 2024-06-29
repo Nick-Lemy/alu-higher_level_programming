c4dac0286347091ef90f44f8ed9f446ef0f27117
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-29 15:32:56 +0200

    Task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 1:
+        print("")
+    else:
+        for i in matrix:
+            if len(i) > 1:
+                for j in i:
+                    print("{}".format(j), end=" " if i[-1] != j else "\n")
+            else:
+                print("{}".format(i), end=" ")
+print_matrix_integer()
