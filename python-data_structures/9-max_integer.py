a2359fb4693eb6bbac1165d37f4538c535cb1a34
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-29 16:18:51 +0200

    Task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
+def max_integer(my_list):
     if len(my_list) == 0:
         return None
     else:
-        sorted(my_list)[-1] 
+       return  sorted(my_list)[-1]
+print(max_integer([1, 90, 2, 13, 34, 5, -13, 3]))
