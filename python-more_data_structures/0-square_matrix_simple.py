2e0f6611405b87f04e8fe5399ee7f5a6f3ca03e2
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-29 18:52:51 +0200

    Task 0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    result = []
+    for row in matrix:
+        name=[]
+        for i in row:
+            name.append(i**2)
+        result.append(name)
+    return result
