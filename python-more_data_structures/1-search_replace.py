ddcf73257dcdd155b330976de93d36949d59b449
Nick-Lemy <n.kayiranga@alustudent.com>
2024-06-29 19:07:50 +0200

    Task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    for i in mylist:
+    for i in my_list:
         if i == search:
             new_list.append(replace)
         else:
