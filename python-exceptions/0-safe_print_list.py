ffb76b73180fe69271608724552ac03753723cb8
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-06 19:07:51 +0200

    Task 0 Attempt 2

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -10,6 +10,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="" if i < b - 1 else "\n")
             a += 1
     except :
-        return a
+        pass
     finally:
         return a
