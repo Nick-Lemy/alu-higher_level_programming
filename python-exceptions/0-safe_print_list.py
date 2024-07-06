89e2a5aa226307ff448933d9d27f9268e317adb3
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-06 19:11:40 +0200

    Task 0 Attempt 3

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,15 +1,10 @@
-#!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    b = 0
-    for i in my_list:
-        b+=1
-    a = 0
+    count = 0
     try:
-        for i in range(0, x):
-            print(my_list[i], end="" if i < b - 1 else "\n")
-            a += 1
-    except :
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
         pass
-    finally:
-        return a
+    print("")
+    return count
