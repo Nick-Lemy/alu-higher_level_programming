d45eda175e7296ed46501c38360b4bc1dfc20f14
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-13 11:04:54 +0200

    End

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """ Create an empty class Square that defines a square """
-
-
 class Square:
-        """ Create an empty class Square that defines a square """
-            pass
+    """ Create an empty class Square that defines a square """
+    pass
