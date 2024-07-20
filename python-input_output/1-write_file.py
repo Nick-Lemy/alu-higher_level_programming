6a9e0094b6371a1db58d690a6132b158b8f05ea8
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 02:22:19 +0200

    Task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Write file in python
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Function that writes a string to a text file (UTF8) and returns the number of characters written
+    """
+
+    a=0
+    with open(filename, 'w') as f:
+        f.write(text, '\n')
+        for i in text:
+            a+=1
+        return a
