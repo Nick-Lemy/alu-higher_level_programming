c2f45027735a209f262568c7eb8fa8e588629087
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 03:36:05 +0200

    Usage of with

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,4 +9,5 @@ def save_to_json_file(my_obj, filename):
     """
     Function that writes an Object to a text file, using a JSON representation
     """
-    json.dump(my_obj, filename)
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
