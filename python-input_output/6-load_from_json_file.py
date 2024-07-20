e8f9bdb0363b130fb275f5495c0204ee88e3c35a
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 03:44:39 +0200

    Task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Create object from a JSON file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Function that creates an Object from a “JSON file”
+    """
+    with open(filename, 'r') as f:
+        json.load(f)
