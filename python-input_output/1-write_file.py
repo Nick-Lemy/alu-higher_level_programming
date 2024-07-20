f6ed4129ac3a1c0ba2a0882bf7d786fb0d25953c
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 02:27:08 +0200

    PEP8 Error fixed

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,12 +6,12 @@ Write file in python
 
 def write_file(filename="", text=""):
     """
-    Function that writes a string to a text file (UTF8) and returns the number of characters written
+    Function that writes a string to a text file
     """
 
-    a=0
+    a = 0
     with open(filename, 'w') as f:
         f.write(f'{text}')
         for i in text:
-            a+=1
+            a += 1
         return a
