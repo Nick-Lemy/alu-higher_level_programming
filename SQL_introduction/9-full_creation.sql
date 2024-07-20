0636c34cfc52751b6b0a1e9c1d1d1354580afee8
Nick-Lemy <n.kayiramga@alustudent.com>
2024-07-20 23:44:39 +0200

    Task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -3,3 +3,8 @@ CREATE TABLE IF NOT EXISTS second_table (
 id INT,
 name VARCHAR(256),
 score INT);
+INSERT INTO second_table VALUES
+(1, "Jonh", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
