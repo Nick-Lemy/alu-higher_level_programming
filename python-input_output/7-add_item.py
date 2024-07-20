dd8b9e76f3dc345fcf386bebd2e8699ec1804b24
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 13:07:56 +0200

    Nothing

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -13,8 +13,9 @@ if __name__ == "__main__":
         loadFile = load_from_json_file("add_item.json")
     except FileNotFoundError:
         loadFile = []
-
-    argc = len(sys.argv)
-    for idx in range(1, argc):
-        loadFile.append(sys.argv[idx])
+    
+    loadFile = sys.argv[1:]
+    # argc = len(sys.argv)
+    # for idx in range(1, argc):
+    #     loadFile.append(sys.argv[idx])
     save_to_json_file(loadFile, "add_item.json")
