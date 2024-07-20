a33a1aceedd8cd69372aeb40cfc2d0a3eaf252c6
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 04:25:12 +0200

    End

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
+"""adds all arguments to a Python list, and then save them to a file:"""
+
+
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
-"""
-7. Load, add, save
-"""
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file') \
+        .load_from_json_file
 
-liste = sys.argv[1:]
-with open('add_item.json', 'w') as f:
-    save_to_json_file(liste, f)
-    load_from_json_file(f)
+    try:
+        loadFile = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        loadFile = []
+
+    argc = len(sys.argv)
+    for idx in range(1, argc):
+        loadFile.append(sys.argv[idx])
+    save_to_json_file(loadFile, "add_item.json")
