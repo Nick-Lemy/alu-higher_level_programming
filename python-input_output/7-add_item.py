4392962a127a2bd7894b55f1271995ffce7f6d46
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 13:10:23 +0200

    There it is

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""adds all arguments to a Python list, and then save them to a file:"""
+"""Are you docuemnted ?"""
 
 
 import sys
 
-if __name__ == "__main__":
+if __name__ == "_main_":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file') \
         .load_from_json_file
@@ -13,9 +13,8 @@ if __name__ == "__main__":
         loadFile = load_from_json_file("add_item.json")
     except FileNotFoundError:
         loadFile = []
-    
-    loadFile = sys.argv[1:]
-    # argc = len(sys.argv)
-    # for idx in range(1, argc):
-    #     loadFile.append(sys.argv[idx])
+
+    argc = len(sys.argv)
+    for idx in range(1, argc):
+        loadFile.append(sys.argv[idx])
     save_to_json_file(loadFile, "add_item.json")
