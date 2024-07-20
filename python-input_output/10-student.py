dcf52ef5c701629df9f58b8722e353503bd77644
Nick-Lemy <n.kayiramga@alustudent.com>
2024-07-20 14:08:55 +0200

    Hey There

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Student to JSON with filter
+"""
+
+
+class Student:
+    """
+    Student Class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
+
