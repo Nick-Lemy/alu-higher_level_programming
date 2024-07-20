ae4caf2f7a908cc2bd155d56dd103c9e4fceda24
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 04:10:14 +0200

    Task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Load, add, save
+"""
+import json
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+
+liste = sys.argv[1:]
+with open('add_item.json', 'w') as f:
+    save_to_json_file(liste, f)
+    load_from_json_file(f)
