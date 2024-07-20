3efb82427df9cf8471eadffc85c0b20a8a4bf87f
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 09:47:05 +0200

    There we go

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,7 +8,11 @@ if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    loadFile = load_from_json_file("add_item.json")
+    try:
+        loadFile = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        loadFile = []
+
     argc = len(sys.argv)
     for idx in range(1, argc):
         loadFile.append(sys.argv[idx])
--- python-input_output/README.md
+++ python-input_output/README.md
@@ -1,2 +1,1 @@
 # Python - Input/Output
-mnmsnd
