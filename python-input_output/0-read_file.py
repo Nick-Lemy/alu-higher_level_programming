e3e04ac3960bcc137fe81cabbf1d90681624138a
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 02:12:21 +0200

    PEP8 validation error correct

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,9 +4,9 @@
 Simple Python file with a fuction that open files
 """
 
-def read_file(filename=""):
+def read_file(filename = ""):
     """
     Function That opens files with UTF8 encoding
     """
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read(), end="")
+        print(f.read(), end = "")
