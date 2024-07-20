f86206deaf66f747b3f15370e8bb448e4ca55152
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 02:10:31 +0200

    Module not documented error solved

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""
+Simple Python file with a fuction that open files
+"""
+
 def read_file(filename=""):
+    """
+    Function That opens files with UTF8 encoding
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         print(f.read(), end="")
