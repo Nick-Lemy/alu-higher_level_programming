d42449447f62a01bd8b32c877abd09aa0da9d807
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 04:15:24 +0200

    n

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""
-7. Load, add, save
-"""
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+"""
+7. Load, add, save
+"""
 
 
 liste = sys.argv[1:]
