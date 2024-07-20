11be0207a881a9782f2a06afe5bb50efb653c438
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 03:19:52 +0200

    Task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+
+"""
+To JSON string
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Function that returns the JSON representation of a string
+    """
+    return json.dumps(my_obj)
