6d02ec3c0726f9de27c9b7cca7959745a5cad2f1
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 04:12:16 +0200

    hey

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 """
 Load, add, save
 """
 import sys
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
