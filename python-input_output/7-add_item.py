f2a0d74090b1a873bd31f935f7974ce5c90bb4b0
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 04:14:14 +0200

    End of Task 7 commits

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-import sys
 """
 7. Load, add, save
 """
-
-
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+
 
 liste = sys.argv[1:]
 with open('add_item.json', 'w') as f:
