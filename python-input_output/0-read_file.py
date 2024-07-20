e962c4dd1ff06d0934b0864cc3a620e88c0f5404
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 02:15:55 +0200

    Now it's done!

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,8 @@
 """
 Simple Python file with a fuction that open files
 """
+
+
 def read_file(filename=""):
     """ Function That opens files with UTF8 encoding """
     with open(filename, 'r', encoding="utf-8") as f:
