bdc02f874868c33a1c151dc5ccfd838b1b2b6033
Nick-Lemy <n.kayiramga@alustudent.com>
2024-07-20 14:25:15 +0200

    Lets go

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-"""
-Student to JSON with filter
-"""
+'''class that makes a student'''
 
 
 class Student:
-    """
-    Student Class
-    """
+    '''student class'''
+    first_name = None
+    last_name = None
+    age = None
+
     def __init__(self, first_name, last_name, age):
+        '''inits the data needed'''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if attrs is None:
-            return self.__dict__
-         if isinstance(attrs, list):
-            if all(isinstance(attr, str) for attr in attrs):
-                result = {}
-                for attr in attrs:
-                    if hasattr(self, attr):
-                        result[attr] = getattr(self, attr)
-                return result
-
-        return self.__dict__
-
+        '''returns a dict rep of the data'''
+        context = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
+        if attrs is None or type(attrs) != list:
+            return context
+        else:
+            cont = {}
+            for item in attrs:
+                if type(item) != str:
+                    return context
+                if item in context.keys():
+                    cont[item] = context[item]
+            return cont
