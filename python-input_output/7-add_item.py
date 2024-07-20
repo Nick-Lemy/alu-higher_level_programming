094cf9b4bbb870cf6e55cd66574f6efc00244214
Nick-Lemy <n.kayiranga@alustudent.com>
2024-07-20 13:14:19 +0200

    There we go

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Are you docuemnted ?"""
+"""adds all arguments to a Python list, and then save them to a file:"""
 
 
 import sys
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file') \
         .load_from_json_file
