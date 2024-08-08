ef92b9f36532b97e57116411d41d702455f5b5c7
Nick-Lemy <n.kayiramga@alustudent.com>
2024-08-08 15:00:02 +0200

    ok

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import urllib.request
 
-url = "https://alu-intranet.hbtn.io/status"
+url = "http://0.0.0.0:5050/status"
 
-with urllib.request.urlopen(url) as response:
-    content = response.read()
+try:
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
 
-print("Body response:")
-print("\t- type: {}".format(type(content)))
-print("\t- content: {}".format(content))
-print("\t- utf8 content: {}".format(content.decode('utf-8')))
-
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode('utf-8')))
+except urllib.error.URLError as e:
+    print("[stderr]:", e.reason)
