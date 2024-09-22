09f1d8b3d1daa3d841332507afb528e45aa7c599
Nick-Lemy <n.kayiranga@alustudent.com>
2024-09-22 19:03:41 +0200

    error fixed

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -7,4 +7,4 @@ for (let i = argv.length - 1; i > 1; i--) {
   liste.push(Math.floor(argv[i]));
 }
 liste.sort(function (a, b) { return a - b; });
-console.log(liste[liste.length - 1]);
+console.log(liste[liste.length - 2]);
