0e73d4b67ecbc20cac15969167f98e9c2d0a91d7
Nick-Lemy <n.kayiranga@alustudent.com>
2024-09-22 19:02:40 +0200

    Task 11

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+const liste = [0];
+
+for (let i = argv.length - 1; i > 1; i--) {
+  liste.push(Math.floor(argv[i]));
+}
+liste.sort(function (a, b) { return a - b; });
+console.log(liste[liste.length - 1]);
