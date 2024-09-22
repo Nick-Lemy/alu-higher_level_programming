ff58d467c4ec936b11726bf4d21b79d7f42d49ea
Nick-Lemy <n.kayiranga@alustudent.com>
2024-09-22 15:11:37 +0200

    Task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,6 +4,8 @@ const { argv } = require('node:process');
 
 if (argv.length < 3) {
   console.log('No argument');
+} else if (argv.length > 3) {
+  console.log('Arguments found');
 } else {
   console.log('Argument found');
 }
