aceaf6bc9aa48cd0c94d556685cf7414d326c434
Nick-Lemy <n.kayiranga@alustudent.com>
2024-09-22 15:18:53 +0200

    Semistandard error fixed

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const { argv } = require('node:process');
-if (argv[2]){
-    console.log(argv[2])
+if (argv[2]) {
+  console.log(argv[2]);
 } else {
-    console.log("No argument")
+  console.log('No argument');
 }
