40e260929857c6e570e6da88ce093597bc5a3ad0
Nick-Lemy <n.kayiranga@alustudent.com>
2024-09-22 16:41:56 +0200

    Task 8

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+let number = argv[2];
+x = '';
+if (number) {
+  for (let i = number; i > 0; i--) {
+    x += 'X';
+  }
+  while (number > 0) {
+    console.log(x);
+    number--;
+  }
+} else {
+  console.log('Missing size');
+}
