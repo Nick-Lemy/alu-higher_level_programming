8e89efc67ae23dfb5e4fa33761d6ee9fe38a9634
Nick-Lemy <n.kayiranga@alustudent.com>
2024-09-22 18:04:57 +0200

    c'est bon

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,5 +1,14 @@
 #!/usr/bin/node
 
+const fact = parseInt(process.argv.slice(2)[0]);
+
+if (isNaN(fact) === true) {
+  console.log(1);
+} else {
+  const result = Factorial(fact);
+  console.log(result);
+}
+
 function Factorial (num) {
   if (num === 1) {
     return 1;
@@ -7,6 +16,3 @@ function Factorial (num) {
     return (num * Factorial(num - 1));
   }
 }
-
-const { argv } = require('node:process');
-console.log(Factorial(argv[2]));
