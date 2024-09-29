a015d2a08b80dd6a5186524d4a84141fd09677ef
Nick-Lemy <n.kayiranga@alustudent.com>
2024-09-29 23:38:58 +0200

    Task 4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (h <= 0 || w <= 0) {
+      return this;
+    } else if (h === undefined || w === undefined) {
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let x = '';
+    let i = this.width;
+    let j = this.height;
+    for (i; i > 0; i--) {
+      x += 'X';
+    }
+    for (j; j > 0; j--) {
+      console.log(x);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
