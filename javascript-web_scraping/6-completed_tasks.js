94dbb2bdf8e424954f1dbbe8a220944e2ca56b16
Nick-Lemy <n.kayiranga@alustudent.com>
2024-10-05 16:08:55 +0200

    Refactor code for better readability and error handling

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -5,21 +5,21 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else {
-        const tasks = JSON.parse(body);
-        const completedTasks = {};
+  if (error) {
+    console.log(error);
+  } else {
+    const tasks = JSON.parse(body);
+    const completedTasks = {};
 
-        tasks.forEach(task => {
-            if (task.completed) {
-                if (!completedTasks[task.userId]) {
-                    completedTasks[task.userId] = 0;
-                }
-                completedTasks[task.userId]++;
-            }
-        });
+    tasks.forEach(task => {
+      if (task.completed) {
+        if (!completedTasks[task.userId]) {
+          completedTasks[task.userId] = 0;
+        }
+        completedTasks[task.userId]++;
+      }
+    });
 
-        console.log(completedTasks);
-    }
+    console.log(completedTasks);
+  }
 });
