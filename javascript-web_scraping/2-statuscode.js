1784c2fdaf3a1aca306c812d7e09192fa216dbd8
Nick-Lemy <n.kayiranga@alustudent.com>
2024-10-05 14:14:29 +0200

    Task 2

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log('code:', response.statusCode);
+    }
+});
