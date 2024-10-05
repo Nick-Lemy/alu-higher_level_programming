1fa257677462b07db98b5f4baf8d353290c8e63d
Nick-Lemy <n.kayiranga@alustudent.com>
2024-10-05 14:16:17 +0200

    code semistandarized

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -5,9 +5,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log('code:', response.statusCode);
-    }
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
 });
